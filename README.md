7d6959cbe9971afbfd05066603691e9cc3e1732a
Jason0409-chen <543181918@qq.com>
2020-08-11 15:10:48 +0800

    init gshop

---
+++ README.md
@@ -0,0 +1,39 @@
+# gshop
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+打包发布
+npm run build
+npm install -g serve
+serve dist
+
+2.4 项目源码目录
+
+2.5 Stylus
+npm install stylus stylus-loader --save-dev
+
+编写样式
+
+2.7 移动端
+view port
+
+2.8 引入vue-router
+npm install vue-router --save
---
+++ src/api/ajax.js
@@ -0,0 +1,37 @@
+/* 
+ajax请求函数模块
+返回promise对象，异步返回的数据是:response.data
+npm install --save axios
+ */
+import axios from 'axios'
+
+export default function ajax(url, data = {}, type = 'GET') {
+	return new Promise(function(resolve, reject) {
+		//执行异步ajax请求
+		let promise
+		if (type === 'GET') {
+			//准备url query参数数据
+			let dataStr = ''
+			Object.keys(data).forEach(key => {
+				dataStr += key + '=' + data[key] + '&'
+			})
+			if (dataStr !== '') {
+				dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+				url = url + '?' + dataStr
+			}
+			//发送get
+			promise = axios.get(url)
+		} else {
+			//发送post
+			promise = axios.post(url, data)
+		}
+		promise.then(function(response) {
+			//成功调用resolve()
+			resolve(response.data)
+		}).catch(function(error) {
+			//失败调用reject()
+			reject(error)
+		})
+		
+	})
+}
